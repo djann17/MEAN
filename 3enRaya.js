e090450e65fad69d9248d2e023125b547a5e89a4
G03-EQ07\tarde <dajar17@yahoo.es>
2016-12-21 20:47:06 +0100

    Miercoles 21

--- 3enRaya.js
+++ 3enRaya.js
@@ -12,9 +12,37 @@ function drop(ev) {
   ev.target.appendChild(document.getElementById(data));
 }
 
-function comenzar(){
-  $("#ficha").show();
-  var txt="<img id='o1' src='circle.png' draggable='true' ondragstart='drag(event)'>";
-  $("#celdafich").html(txt);
+function jugar(){
+	if (ronda==1){
+	  $("#btcom").val("Fin de turno");
+	    $("#ficha").show();
+	}		
+	if (ronda<=6)
+	  inificha();
+    if (ronda==7)
+	    $("#ficha").hide();
+	if (ronda%2!=0)
+	  $("#mensaje").html("Tu turno Jugador 1");
+     else
+	  $("#mensaje").html("Tu turno Jugador 2");	
+	ronda++;
+	
 }
 	
+function inificha(){
+  var jug;
+  var ntur;
+  var nimg;
+  switch (ronda){
+    case 1: jug="o"; ntur=1; nimg="circle.png"; break;
+	case 2: jug="x"; ntur=1; nimg="x.png"; break;
+	case 3: jug="o"; ntur=2; nimg="circle.png"; break;
+	case 4: jug="x"; ntur=2; nimg="x.png"; break;
+	case 5: jug="o"; ntur=3; nimg="circle.png"; break;
+	case 6: jug="x"; ntur=3; nimg="x.png"; break;
+  }
+
+  var txt="<img id='" + jug + ntur + "' src='" + nimg + "' draggable='true' ondragstart='drag(event)'>";
+  $("#celdafich").html(txt);
+
+}
